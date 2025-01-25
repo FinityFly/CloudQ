75a6a5a9fbcf3e42574cb8141b886da186faea0a
Juno9170 <ryanzhuturtle@gmail.com>
2025-01-25 16:17:04 -0500

    remove sidebar for join line

--- frontend/src/pages/JoinEventLine.tsx
+++ frontend/src/pages/JoinEventLine.tsx
@@ -1,24 +1,9 @@
 import React from 'react'
-import Layout from '@/Layout'
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
 import { JoinEvent } from '@/components/JoinEvent'
-const sideBarItems = [
-  {
-    title: "Home",
-    url: "/",
-    icon: Home,
-  },
-  {
-    title: "Create Event",
-    url: "/create-event",
-    icon: Calendar,
-  }
-]
 const JoinEventLine = () => {
   return (
-    <Layout items={sideBarItems}>
         <JoinEvent/>
-    </Layout>
+
   )
 }
 
